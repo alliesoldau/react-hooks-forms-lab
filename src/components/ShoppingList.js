13687023474a2aa33685796d032f06ea6e502dd2
Allie Soldau <alliesoldau@gmail.com>
2022-11-23 11:13:57 -0500

    first deliverable done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,24 +5,37 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
+    console.log(event.target.value);
   }
 
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
+    if (item.name.toLowerCase().includes(search.toLowerCase())) {
+      if (selectedCategory === "All") {
+        return true;
+      } else if (item.category === selectedCategory) {
+        return true;
+      }
+    }
   });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter 
+        search={search}
+        onSearchChange={setSearch}
+        onCategoryChange={handleCategoryChange} 
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+          <Item key={item.id} 
+            name={item.name} 
+            category={item.category} 
+          />
         ))}
       </ul>
     </div>
